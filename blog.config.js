d1579c21d036a3e6db07dd7fa9e7a70c28b17999
Wiobr <hucige@gmail.com>
2024-05-29 22:00:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '✨智桥•Share✨', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '爱探索，爱分享📚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wob-share.pages.dev/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.qmshare.top/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '智桥分享, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
